9e240984149c4887a8280d04d0ba72463cb587f3
Musa SAYAR <musasayar67@gmail.com>
2023-02-28 16:25:03 +0300

    productırem düzenlendi

--- src/componet/ProductItem.js
+++ src/componet/ProductItem.js
@@ -113,12 +113,12 @@ const basketProduct = basket.find(item => item.id === product.id )
      
         <picture className='relative mt-2 md:mt-[3px] lg:mt-[5px] shrink-0 cursor-pointer'>
         {
-          !like ? <button className=' z-10 mt-2 w-8 h-8 md:w-10 md:h-10  top-2 right-2 lg:top-4 lg:right-6 border 
+          !like ? <button className=' z-10 mt-2 w-8 h-8 md:w-10 md:h-10  top-2 right-8 lg:top-4 lg:right-6 border 
           border-gray-300 float-right
           hover:text-white hover:bg-pink-600 hover:border-transparent 
           focus:outline-none focus:ring-2 focus:ring-pink-600 focus:ring-offset-2
-           items-center justify-center flex md:absolute  rounded-full drop-shadow-xl' onClick ={favouriteProduct}><BsSuitHeart size={20}/></button> 
-          : <button className='float-right  z-10 mt-2 w-8 h-8 md:w-10 md:h-10  top-2 right-2 lg:top-4 lg:right-6 border  border-gray-200  items-center justify-center flex md:absolute rounded-full drop-shadow-xl ' onClick={favouriteTry}><BsFillSuitHeartFill size={20} className='text-pink-700'/></button>
+           items-center justify-center flex absolute  rounded-full drop-shadow-xl' onClick ={favouriteProduct}><BsSuitHeart size={20}/></button> 
+          : <button className='float-right  z-10 mt-2 w-8 h-8 md:w-10 md:h-10  top-2 right-8 lg:top-4 lg:right-6 border  border-gray-200  items-center justify-center flex absolute rounded-full drop-shadow-xl ' onClick={favouriteTry}><BsFillSuitHeartFill size={20} className='text-pink-700'/></button>
           
         } 
          
